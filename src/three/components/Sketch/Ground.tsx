b580cc187af8475521a68665fe50a509a49aeec7
More <765830637@qq.com>
2024-11-23 00:37:55 +0800

    chore: delete the test code

--- src/three/components/Sketch/Ground.tsx
+++ src/three/components/Sketch/Ground.tsx
@@ -4,18 +4,13 @@ import groundVertexShader from "../Shader/ground/vertex.glsl";
 import groundFragmentShader from "../Shader/ground/fragment.glsl";
 import { useTexture } from "@react-three/drei";
 import RES from "../RES";
-import { useInteractStore } from "@utils/Store";
-import { ThreeEvent, useThree } from "@react-three/fiber";
+
 
 const Ground = () => {
   const diffuseTex = useTexture(RES.textures.grid);
   diffuseTex.wrapS = diffuseTex.wrapT = RepeatWrapping;
 
   const groundTex = useTexture(RES.textures.ground);
-
-  const controlDom = useInteractStore((state) => state.controlDom);
-
-  const events = useThree((state) => state.events);
 
   const uniforms = useMemo(
     () => ({
@@ -24,20 +19,12 @@ const Ground = () => {
     }),
     []
   );
-
-  useEffect(() => {
-    events.connect!(controlDom);
-  });
-
-  const handlePointerDown = (event: ThreeEvent<PointerEvent>) => {
-  };
-
   return (
     <>
       <mesh
         rotation-x={-Math.PI / 2}
         scale={50}
-        onPointerDown={handlePointerDown}
+
       >
         <planeGeometry args={[1, 1, 128, 128]} />
         <shaderMaterial
