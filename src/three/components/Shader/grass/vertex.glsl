449652d88f0de4930cddc54cb63ac8d4ed9720f8
More <765830637@qq.com>
2024-11-25 18:22:13 +0800

    fix: rotateY matrix error

--- src/three/components/Shader/grass/vertex.glsl
+++ src/three/components/Shader/grass/vertex.glsl
@@ -39,7 +39,7 @@ float easeOut(float x, float t) {
 mat3 rotateY(float theta) {
   float c = cos(theta);
   float s = sin(theta);
-  return mat3(vec3(c, 0, s), vec3(0, 1, 0), vec3(-s, 0, c));
+  return mat3(vec3(c, 0, -s), vec3(0, 1, 0), vec3(s, 0, c));
 }
 
 mat3 rotateAxis(vec3 axis, float angle) {
